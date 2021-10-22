e1dfb8721f37ffe240164ec4e5e0f1cedfa13607
drew <andry.marini@gmail.com>
2021-10-22 13:07:54 +0200

    aggiunto progetto con tab

---
+++ MC-tabbed/MC-tabbed/MC-tabbed/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MC_tabbed.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MC-tabbed/MC-tabbed/MC-tabbed/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MC_tabbed.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace MC_tabbed.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
