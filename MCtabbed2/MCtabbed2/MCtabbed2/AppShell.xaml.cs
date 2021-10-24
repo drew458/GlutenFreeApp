17e57663d9af61fd889e7dc7262987d817136785
drew <andry.marini@gmail.com>
2021-10-24 17:43:27 +0200

    Aggiunto tab con mappa inizializzata (manca google maps api key) https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/map/setup https://docs.microsoft.com/en-us/xamarin/xamarin-forms/user-interface/map/pins

---
+++ MCtabbed2/MCtabbed2/MCtabbed2/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using MCtabbed2.ViewModels;
+using MCtabbed2.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace MCtabbed2
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
