956803fe4e2e6957ffaef19d32226bcdfe044da4
drew <andry.marini@gmail.com>
2021-10-26 17:36:29 +0200

    aggiunta inizializzazione lista

---
+++ MCtabbed2/MCtabbed2/MCtabbed2/Models/Regione.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCtabbed2.Models
+{
+    public class Regione
+    {
+
+        public string Nome { get; set; }
+        public string Immagine { get; set; }
+
+        public Regione(string Nome, string Immagine)
+        {
+            this.Nome = Nome;
+            this.Immagine = Immagine;
+        }
+    }
+}
---
+++ MCtabbed2/MCtabbed2/MCtabbed2/ViewModels/ListViewModel.cs
@@ -0,0 +1,43 @@
+﻿using MCtabbed2.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCtabbed2.ViewModels
+{
+    public class ListViewModel : BaseViewModel
+    {
+        public List<Regione> Regioni { get; set; }
+
+        public ListViewModel()
+        {
+            Title = "List (regioni)";
+
+            var immagine = "";
+
+            Regioni = new List<Regione>
+            {
+                new Regione("Abruzzo", ""),
+                new Regione("Basilicata", ""),
+                new Regione("Calabria", ""),
+                new Regione("Campania", ""),
+                new Regione("Emilia Romagna", ""),
+                new Regione("Friuli Venezia Giulia", ""),
+                new Regione("Lazio", ""),
+                new Regione("Liguria", ""),
+                new Regione("Lombardia", ""),
+                new Regione("Marche", ""),
+                new Regione("Molise", ""),
+                new Regione("Piemonte", ""),
+                new Regione("Puglia", ""),
+                new Regione("Sardegna", ""),
+                new Regione("Sicilia", ""),
+                new Regione("Toscana", ""),
+                new Regione("Trentino Alto Adige", ""),
+                new Regione("Umbria", ""),
+                new Regione("Valle d'Aosta", ""),
+                new Regione("Veneto", "")
+            };
+        }
+    }
+}
