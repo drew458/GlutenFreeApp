c61825b9df184f65d9dbdcfe36188846294dd002
drew <andry.marini@gmail.com>
2021-11-06 14:38:39 +0100

    allineati immagini e testo nella lista delle regioni

--- MCtabbed2/MCtabbed2/MCtabbed2/Views/RegioniPage.xaml.cs
+++ MCtabbed2/MCtabbed2/MCtabbed2/Views/RegioniPage.xaml.cs
@@ -12,14 +12,6 @@ namespace MCtabbed2.Views
         public RegioniPage()
         {
             InitializeComponent();
-
-            // START DEBUG CODE
-            var assembly = typeof(RegioniPage).GetTypeInfo().Assembly;
-            foreach (var res in assembly.GetManifestResourceNames())
-            {
-                System.Diagnostics.Debug.WriteLine("found resource: " + res);
-            }
-            // END DEBUG CODE
         }
         async void OnCollectionViewSelectionChanged(object sender, SelectionChangedEventArgs e)
         {
