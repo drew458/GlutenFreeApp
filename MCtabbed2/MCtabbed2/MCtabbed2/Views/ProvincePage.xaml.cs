80ea97bb082ef897e864b55100e1517eb173a78d
drew <andry.marini@gmail.com>
2021-11-16 19:50:20 +0100

    navigazioni regioni - province funziona e l'app non crasha, ma non vengono visualizzate le province

--- MCtabbed2/MCtabbed2/MCtabbed2/Views/ProvincePage.xaml.cs
+++ MCtabbed2/MCtabbed2/MCtabbed2/Views/ProvincePage.xaml.cs
@@ -10,10 +10,6 @@ namespace MCtabbed2.Views
     [QueryProperty(nameof(Nome), "name")]
     public partial class ProvincePage : ContentPage
     {
-        public ProvincePage()
-        {
-            InitializeComponent();
-        }
 
         public string Nome
         {
@@ -23,19 +19,24 @@ namespace MCtabbed2.Views
             }
         }
 
-        private void LoadProvince(string nome)
+        public ProvincePage()
+        {
+            InitializeComponent();
+        }
+
+        void LoadProvince(string nome)
         {
             try
             {
                 Regione regione = RegioniData.Regioni.FirstOrDefault(r => r.Nome == nome);
                 IList<Provincia> province = regione.Province;
                 BindingContext = province;
+                
             }
             catch (Exception)
             {
                 Console.WriteLine("Impossibile caricare la provincia");
             }
-
-        }        
+        }
     }
 }
