4e620594d775caf48ff97049224b6a10d1639ef3
drew <andry.marini@gmail.com>
2021-12-10 16:06:49 +0100

    ricerca delle province funziona

--- MCtabbed2/MCtabbed2/MCtabbed2/Views/ProvincePage.xaml.cs
+++ MCtabbed2/MCtabbed2/MCtabbed2/Views/ProvincePage.xaml.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System;
 using System.Collections.Generic;
 using MCtabbed2.ViewModels;
+using System.Collections;
 
 namespace MCtabbed2.Views
 {
@@ -14,6 +15,36 @@ namespace MCtabbed2.Views
         {
             InitializeComponent();
             BindingContext = new ProvinceViewModel();
+        }
+
+
+        /* from https://www.youtube.com/watch?v=_YefBlDAUHA & http://xamaringuyshow.com/2018/12/08/xamrin-forms-search-item-on-listview-mvvm-easy-approach/
+         * void Handle_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var _container = BindingContext as ProvinceViewModel;
+            //ProvinceCollectionView.BeginRefresh();
+
+            if (string.IsNullOrWhiteSpace(e.NewTextValue))
+                ProvinceCollectionView.ItemsSource = _container.ListaProvince;
+            else
+                ProvinceCollectionView.ItemsSource = _container.ListaProvince.Where(i => i.Nome.Contains(e.NewTextValue));
+
+            //EmployeeListView.EndRefresh();
+        } */
+
+        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            ProvinceCollectionView.ItemsSource = GetList(e.NewTextValue);
+        }
+
+        private IEnumerable<Provincia> GetList(string nomeProvincia = null)
+        {
+            ProvinceViewModel _container = BindingContext as ProvinceViewModel;
+            IList<Provincia> province = _container.ListaProvince;
+            
+            return string.IsNullOrEmpty(nomeProvincia) ? province : province
+                .Where(p => p.Nome.ToLower()
+                .StartsWith(nomeProvincia.ToLower()));
         }
     }
 }
