023b08c63d937595830199ea1d6e2cc0f7346aa5
drew <andry.marini@gmail.com>
2021-12-15 23:12:17 +0100

    aggiunto progetto WebApi e Shared

---
+++ MCtabbed2/MCtabbed2.Shared/MCtabbed2.Shared/Models/Falesia.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms.Maps;
+
+namespace MCtabbed2.Models
+{
+    public class Falesia
+    {
+        public string Nome { get; set; }
+        public string Indirizzo { get; set; }
+        public int NumeroVie { get; set; }
+        public int VieConRipetizioni { get; set; }
+        public int NumeroRipetizioni { get; set; }
+        public Position Posizione { get; set; }
+    }
+}
