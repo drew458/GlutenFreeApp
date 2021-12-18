16d240f61b68b0e4219c85763a6173016b94f63f
drew <andry.marini@gmail.com>
2021-12-18 19:24:08 +0100

    aggiunta funzione base

---
+++ MCtabbed2/RDSQuery/handler.py
@@ -0,0 +1,31 @@
+import pymysql
+import os
+ 
+# 1. Install pymysql to local directory
+# pip install -t $PWD pymysql
+ 
+# 2. (If Using Lambda) Write your code, then zip it all up 
+# a) Mac/Linux --> zip -r9 ${PWD}/function.zip 
+# b) Windows --> Via Windows Explorer
+ 
+# Lambda Permissions:
+# AWSLambdaVPCAccessExecutionRole
+ 
+#Configuration Values
+endpoint = os.environ["ENDPOINT"]
+username = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
+database_name = os.environ["DBNAME"]
+ 
+#Connection
+connection = pymysql.connect(endpoint, user=username,
+	passwd=password, db=database_name)
+ 
+def lambda_handler(event, context):
+	cursor = connection.cursor()
+	cursor.execute('SELECT * from Falesie')
+ 
+	rows = cursor.fetchall()
+ 
+	for row in rows:
+		print("{0} {1} {2}".format(row[0], row[1], row[2]))
