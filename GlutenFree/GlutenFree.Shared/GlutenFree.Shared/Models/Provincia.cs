5605e32324240203f2c0b3abc58e42d4a425f571
drew <andry.marini@gmail.com>
2021-12-21 15:54:19 +0100

    added models to shared project

---
+++ GlutenFree/GlutenFree.Shared/GlutenFree.Shared/Models/Provincia.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using GlutenFree.Shared.Models;
+
+namespace GlutenFree.Models
+{
+    public class Provincia
+    {
+        public string Nome { get; set; }
+        public IList<Restaurant> Falesie { get; set; }
+    }
+}
