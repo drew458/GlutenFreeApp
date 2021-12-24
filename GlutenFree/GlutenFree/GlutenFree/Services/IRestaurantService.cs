6f0e02fdffbb9857523920ff886dd418d6aa6d4d
drew <andry.marini@gmail.com>
2021-12-24 18:55:58 +0100

    added remote database queries

--- GlutenFree/GlutenFree/GlutenFree/Services/IRestaurantService.cs
+++ GlutenFree/GlutenFree/GlutenFree/Services/IRestaurantService.cs
@@ -6,9 +6,23 @@ namespace GlutenFree.Services
 {
     public interface IRestaurantService
     {
-        Task AddRestaurant(string name);
-        Task<IEnumerable<Restaurant>> GetRestaurants();
-        Task<Restaurant> GetRestaurant(int id);
-        Task DeleteRestaurant(int id);
+        //Task<IEnumerable<Restaurant>> RefreshRestaurantsAsync();
+        Task<IEnumerable<Restaurant>> GetRestaurantsAsync();
+        Task AddRestaurantAsync(string name, string address, string city, string province, string region, 
+            double latitude, double longitude, string dishType, int specialMenu);
+        Task<Restaurant> GetRestaurantAsync(int id);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncName(string name);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncCity(string city);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvince(string province);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegion(string region);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncDishType(string dishType);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncSpecialMenu(int specialMenu);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncCityDishType(string city, string dishType);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncCityDishTypeSpecialMenu(string city, string dishType, int specialMenu);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvinceDishType(string province, string dishType);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvinceDishTypeSpecialMenu(string province, string dishType, int specialMenu);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegionDishType(string region, string dishType);
+        Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegionDishTypeSpecialMenu(string region, string dishType, int specialMenu);
+        Task DeleteRestaurantAsync(int id);
     }
 }
--- GlutenFree/GlutenFree/GlutenFree/Services/RestaurantService.cs
+++ GlutenFree/GlutenFree/GlutenFree/Services/RestaurantService.cs
@@ -25,19 +25,25 @@ namespace GlutenFree.Services
             await db.CreateTableAsync<Restaurant>();
         }
 
-        public Task AddRestaurant(string name)
+        public Task<IEnumerable<Restaurant>> RefreshRestaurantsAsync()
         {
             throw new NotImplementedException();
         }
 
-        public async Task DeleteRestaurant(int id)
+        public async Task<IEnumerable<Restaurant>> GetRestaurantsAsync()
         {
             await Init();
 
-            await db.DeleteAsync<Restaurant>(id);
+            var restaurant = await db.Table<Restaurant>().ToListAsync();
+            return restaurant;
         }
 
-        public async Task<Restaurant> GetRestaurant(int id)
+        public Task AddRestaurantAsync(string name, string address, string city, string province, string region, double latitude, double longitude, string dishType, int specialMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Restaurant> GetRestaurantAsync(int id)
         {
             await Init();
 
@@ -47,12 +53,71 @@ namespace GlutenFree.Services
             return restaurant;
         }
 
-        public async Task<IEnumerable<Restaurant>> GetRestaurants()
+        public async Task DeleteRestaurantAsync(int id)
         {
             await Init();
 
-            var restaurant = await db.Table<Restaurant>().ToListAsync();
-            return restaurant;
+            await db.DeleteAsync<Restaurant>(id);
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncCity(string city)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvince(string province)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegion(string region)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncDishType(string dishType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncSpecialMenu(int specialMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncCityDishType(string city, string dishType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncCityDishTypeSpecialMenu(string city, string dishType, int specialMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvinceDishType(string province, string dishType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncProvinceDishTypeSpecialMenu(string province, string dishType, int specialMenu)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegionDishType(string region, string dishType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Restaurant>> GetRestaurantAsyncRegionDishTypeSpecialMenu(string region, string dishType, int specialMenu)
+        {
+            throw new NotImplementedException();
         }
     }
 }
