e2918c4e5d1cf57db49247d550f7d54ea4b20802
drew <andry.marini@gmail.com>
2022-02-11 09:51:16 +0100

    Added encryption and user model class

---
+++ GlutenFree/GlutenFree.Login/EncryptionService.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace GlutenFree.Login
+{
+    public static class EncryptionService
+    {
+        public static string Encrypt(string user, string password)
+        {
+            using var encryptionAlgorithm = SHA512.Create();
+
+            byte[] input = Encoding.ASCII.GetBytes(user + password);
+
+            byte[] result = encryptionAlgorithm.ComputeHash(input);
+
+            string hashedString = BitConverter.ToString(result);
+            hashedString = hashedString.Replace("-", "").ToLowerInvariant();
+
+            return hashedString;
+        }
+    }
+}
