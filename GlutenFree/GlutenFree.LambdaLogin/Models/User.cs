39c1666570a99ecbe0ee47fa03a27a11a5677efb
drew <andry.marini@gmail.com>
2022-02-11 16:38:14 +0100

    added VS lambda function

---
+++ GlutenFree/GlutenFree.LambdaLogin/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace GlutenFree.Login.Models
+{
+    class User
+    {
+        public int Id { get; set; }
+        public char Email { get; set; }
+        public char Password { get; set; }
+    }
+}
